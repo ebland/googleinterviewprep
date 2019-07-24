b85f5a77c3202f0a2fc4211eba1e671611757a00
gencomsolar <elizabethblands@gmail.com>
2019-07-24 11:42:18 -0700

    addeded )

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app= require(“express”)()
 app.get(“/“, (req, res) => {
 
-    res.setHeader(“set-cookie”, [“setfromserver=1”]
+    res.setHeader(“set-cookie”, [“setfromserver=1”])
     res.sendFile(‘${__dirname}/index.html’)
-    
+
 })
+
 app.listen(8080, () =>console.log(“listening on port 8080”))
