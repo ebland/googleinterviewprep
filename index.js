dfe36d613049777bbc425d4783241a4fc5105f9f
gencomsolar <elizabethblands@gmail.com>
2019-07-24 11:39:58 -0700

    button fixed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app= require(“express”)()
 app.get(“/“, (req, res) => {
+
     res.setHeader(“set-cookie”, [“setfromserver=1”]
-    res.sendFile(‘${_dirname}/index.html’)
+    res.sendFile(‘${__dirname}/index.html’)
+    
 })
 app.listen(8080, () =>console.log(“listening on port 8080”))
